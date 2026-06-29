88c4ffea9603eca04f908f6eac04d847d49c0371
Denis <dennismarfo@gmail.com>
2026-06-29 17:31:29 -0400

    feat: route projects labs page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import Projects from './Projects';
+
+const Root = window.location.pathname.replace(/\/+$/, '') === '/projects' ? Projects : App;
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 );
